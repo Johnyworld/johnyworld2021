0c99e596db73329dbe56d223896a2d10713ac296
johnyworld <johnyworld@naver.com>
2020-10-07 18:54:08 +0900

    Test server side routing and api with proxy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+  fetch('/api/hello')
+    .then(r=> r.json())
+    .then(data => console.log(data));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' render={() => <p>Home</p>} />
+        <Route path='/test' render={() => <p>Test</p>} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,15 @@
 import express from 'express';
 import api from './api/index';
-import bodyParser from 'body-parser';
 
 const app = express();
 const root = require('path').join(__dirname, '..', 'client', 'build');
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+// app.all('*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
 
-// app.use(bodyParser.json());
 app.use('/api', api);
 
 app.use(express.static(root));
