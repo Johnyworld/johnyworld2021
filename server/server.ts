3c31fe0fe427d952fe34df08f84b559870cbc815
johnyworld <johnyworld@naver.com>
2020-10-07 18:12:18 +0900

    Update server.ts: 정적 빌드 초기 설정

--- server/server.ts
+++ server/server.ts
@@ -1,27 +1,22 @@
 import express from 'express';
 import api from './api/index';
 import bodyParser from 'body-parser';
-const path = require('path');
 
 const app = express();
-const clientApp = path.join(__dirname, 'build');
+const root = require('path').join(__dirname, '..', 'client', 'build');
 
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next();
-// });
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 // app.use(bodyParser.json());
-// app.use('/api', api);;
-app.use(express.static(clientApp));
+app.use('/api', api);
 
-app.use('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
+app.use(express.static(root));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './build', 'index.html')); 
-// });
+app.get("*", (req, res) => { res.sendFile('index.html', { root }); })
 
-const port = 4004;
-// const port = process.env.PORT || 4004;
+const port = process.env.PORT || 4004;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
